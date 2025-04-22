85c963d37edca09934dfdd26d0666be09c8d66ac
kangfenmao <kangfenmao@qq.com>
2025-04-22 21:49:47 +0800

    feat: add sentry integration

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -12,6 +12,7 @@ import { BrowserWindow, ipcMain, session, shell } from 'electron'
 import log from 'electron-log'
 
 import { titleBarOverlayDark, titleBarOverlayLight } from './config'
+import { initSentry } from './integration/sentry'
 import AppUpdater from './services/AppUpdater'
 import BackupManager from './services/BackupManager'
 import { configManager } from './services/ConfigManager'
@@ -344,4 +345,7 @@ export function registerIpc(mainWindow: BrowserWindow, app: Electron.App) {
   ipcMain.handle(IpcChannel.SearchWindow_OpenUrl, async (_, uid: string, url: string) => {
     return await searchService.openUrlInSearchWindow(uid, url)
   })
+
+  // sentry
+  ipcMain.handle(IpcChannel.Sentry_Init, () => initSentry())
 }
